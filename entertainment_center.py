24faca5ae5f939a24db60d6e80fae0d610756bf9
Steven Greulich <greulich.steven@gmail.com>
2017-09-10 07:55:35 -0700

    initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story about a boy and his toys that come to life", "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
